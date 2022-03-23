3beb1ecfa8596297ece469416fce492c6a374bb1
Gergely Padányi-Gulyás <gergely.padanyi-gulyas@dkfkft.hu>
2022-03-23 10:23:11 +0100

    feat(app): main refactor board tests

--- src/app/classes/Board.spec.ts
+++ src/app/classes/Board.spec.ts
@@ -2,63 +2,51 @@ import { BetType } from '../enums/BetType'
 import { Board } from './Board'
 import { RouletteWheel } from './RouletteWheel'
 
+function createSpyBoard(spin: number): Board {
+  const wheelSpy: RouletteWheel = jasmine.createSpyObj('RouletteWheel', {
+    spin
+  })
+  return new Board(wheelSpy)
+}
+
 describe('Board', () => {
-  const rouletteWheelSpy: RouletteWheel = jasmine.createSpyObj(
-    'RouletteWheel',
-    { spin: 0 }
-  )
-  const board = new Board(rouletteWheelSpy)
+  const betMoney = 100
+  const moneyWhenPlayerLost = 0
 
   it('Bet 0 win 35x', () => {
-    expect(board.roll(100, BetType.ONE_NUMBER, 0)).toEqual(3600)
+    const board = createSpyBoard(0)
+    expect(board.roll(betMoney, BetType.ONE_NUMBER, 0)).toEqual(3600)
   })
 
   it('Bet 10 loose', () => {
-    expect(board.roll(300, BetType.ONE_NUMBER, 10)).toEqual(0)
+    const board = createSpyBoard(0)
+    expect(board.roll(betMoney, BetType.ONE_NUMBER, 10)).toEqual(
+      moneyWhenPlayerLost
+    )
   })
 
   it('Bet PASSE win 1x', () => {
-    const rouletteWheelPasseSpy: RouletteWheel = jasmine.createSpyObj(
-      'RouletteWheel',
-      { spin: 19 }
-    )
-    const passeBoard = new Board(rouletteWheelPasseSpy)
-    expect(passeBoard.roll(100, BetType.PASSE)).toEqual(200)
+    const board = createSpyBoard(19)
+    expect(board.roll(betMoney, BetType.PASSE)).toEqual(200)
   })
 
   it('Bet PASSE loose', () => {
-    const rouletteWheelPasseSpy: RouletteWheel = jasmine.createSpyObj(
-      'RouletteWheel',
-      { spin: 18 }
-    )
-    const passeBoard = new Board(rouletteWheelPasseSpy)
-    expect(passeBoard.roll(100, BetType.PASSE)).toEqual(0)
+    const board = createSpyBoard(18)
+    expect(board.roll(betMoney, BetType.PASSE)).toEqual(moneyWhenPlayerLost)
   })
 
   it('Bet MANQUE win 1x', () => {
-    const rouletteWheelManqueSpy: RouletteWheel = jasmine.createSpyObj(
-      'RouletteWheel',
-      { spin: 1 }
-    )
-    const manqueBoard = new Board(rouletteWheelManqueSpy)
-    expect(manqueBoard.roll(100, BetType.MANQUE)).toEqual(200)
+    const board = createSpyBoard(1)
+    expect(board.roll(betMoney, BetType.MANQUE)).toEqual(200)
   })
 
   it('Bet MANQUE loose with 0', () => {
-    const rouletteWheelManqueSpy: RouletteWheel = jasmine.createSpyObj(
-      'RouletteWheel',
-      { spin: 0 }
-    )
-    const manqueBoard = new Board(rouletteWheelManqueSpy)
-    expect(manqueBoard.roll(100, BetType.MANQUE)).toEqual(0)
+    const board = createSpyBoard(0)
+    expect(board.roll(betMoney, BetType.MANQUE)).toEqual(moneyWhenPlayerLost)
   })
 
   it('Bet MANQUE loose with 19', () => {
-    const rouletteWheelManqueSpy: RouletteWheel = jasmine.createSpyObj(
-      'RouletteWheel',
-      { spin: 19 }
-    )
-    const manqueBoard = new Board(rouletteWheelManqueSpy)
-    expect(manqueBoard.roll(100, BetType.MANQUE)).toEqual(0)
+    const board = createSpyBoard(19)
+    expect(board.roll(betMoney, BetType.MANQUE)).toEqual(moneyWhenPlayerLost)
   })
 })
