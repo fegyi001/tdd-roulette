57269c49f0ecf0bdbf8a4e6b24c352c3cfd7ff93
Gergely Padányi-Gulyás <gergely.padanyi-gulyas@dkfkft.hu>
2022-04-11 09:23:39 +0200

    feat(app): main add refactor RouletteError and RouletteValidationError

--- src/app/classes/RouletteError.ts
+++ src/app/classes/RouletteError.ts
@@ -1,13 +1,6 @@
 export class RouletteError extends Error {
-  constructor() {
-    super()
-    Object.setPrototypeOf(this, RouletteError.prototype)
-  }
-}
-
-export class RouletteErrorWithMessage extends Error {
   constructor(msg: string) {
     super(msg)
-    Object.setPrototypeOf(this, RouletteErrorWithMessage.prototype)
+    Object.setPrototypeOf(this, RouletteError.prototype)
   }
 }
--- src/app/classes/RouletteValidationError.ts
+++ src/app/classes/RouletteValidationError.ts
@@ -1,26 +1,27 @@
-import { RouletteError, RouletteErrorWithMessage } from './RouletteError'
+import { RouletteError } from './RouletteError'
 
 export class RouletteValidationError extends RouletteError {
-  constructor() {
-    super()
+  constructor(message: string) {
+    super(message)
     Object.setPrototypeOf(this, RouletteValidationError.prototype)
   }
 }
-export class RouletteValidationErrorWithMessage extends RouletteErrorWithMessage {
-  constructor(message: string) {
-    super(message)
-    Object.setPrototypeOf(this, RouletteValidationErrorWithMessage.prototype)
+
+export class RouletteValidationErrorWithoutMessage extends RouletteValidationError {
+  constructor() {
+    super('')
+    Object.setPrototypeOf(this, RouletteValidationErrorWithoutMessage.prototype)
   }
 }
 
-export class EmptyRouletteValidationError extends RouletteError {
+export class EmptyRouletteValidationError extends RouletteValidationErrorWithoutMessage {
   constructor() {
     super()
     Object.setPrototypeOf(this, EmptyRouletteValidationError.prototype)
   }
 }
 
-export class MissingPropertyRouletteValidationError extends RouletteErrorWithMessage {
+export class MissingPropertyRouletteValidationError extends RouletteValidationError {
   constructor(message: string) {
     super(message)
     Object.setPrototypeOf(
@@ -30,21 +31,21 @@ export class MissingPropertyRouletteValidationError extends RouletteErrorWithMes
   }
 }
 
-export class NotValidIdRouletteValidationError extends RouletteErrorWithMessage {
+export class NotValidIdRouletteValidationError extends RouletteValidationError {
   constructor(message: string) {
     super(message)
     Object.setPrototypeOf(this, NotValidIdRouletteValidationError.prototype)
   }
 }
 
-export class NotValidBetMoneyError extends RouletteError {
+export class NotValidBetMoneyError extends RouletteValidationErrorWithoutMessage {
   constructor() {
     super()
     Object.setPrototypeOf(this, NotValidBetMoneyError.prototype)
   }
 }
 
-export class NotValidBetPlaceError extends RouletteError {
+export class NotValidBetPlaceError extends RouletteValidationErrorWithoutMessage {
   constructor() {
     super()
     Object.setPrototypeOf(this, NotValidBetPlaceError.prototype)
